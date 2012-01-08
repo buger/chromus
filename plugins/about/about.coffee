ce804a819d77db47644dd34f04cd2f0f79073d7e
Leonid Bugaev <leonsbox@gmail.com>
2012-01-08 14:27:29 +0400

    Added option to toggle debug mode
    When in debug it loads all resources directly and did't cache it

--- plugins/about/about.coffee
+++ plugins/about/about.coffee
@@ -7,15 +7,37 @@ template = Handlebars.compile '
         <h1 style="background:url(assets/icons/42x42.png) center left no-repeat; font-weight: bold; font-size: 16px; display: inline-block; line-height: 42px; padding-left: 50px">Chromus v3.0.2</h1>
 
         <a href="https://github.com/chromus/chromus" style="display:block;margin-top:15px; font-size: 14px;" target="_blank">github</a>
+
+        <div>
+            <label><input type="checkbox" name="debug" {{#if debug}}checked="checked"{{/if}}/> Debug mode</label>            
+        </div>
     </div>
 '
+
+class About extends Backbone.View
+
+    events:
+        "change input[name='debug']": 'toggleDebug'
+
+    initialize: ->
+        chromus.openPanel @el
+        @render()
+
+    render: ->
+        view =
+            debug: store.get('debug')
+                    
+        @el.innerHTML = template(view)
+
+    toggleDebug: (el) ->
+        store.set('debug', el.checked)
 
 
 about = $('<li>About</li>')
     .bind 'click', ->
         $('#main_menu').hide()
 
-        chromus.openPanel template()
+        new About()
 
 
 chromus.addMenu about
--- plugins/about/about.js
+++ plugins/about/about.js
@@ -1,5 +1,6 @@
 (function() {
-  var about, template;
+  var About, about, template;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   template = Handlebars.compile('    \
     <header>\
@@ -10,12 +11,49 @@
         <h1 style="background:url(assets/icons/42x42.png) center left no-repeat; font-weight: bold; font-size: 16px; display: inline-block; line-height: 42px; padding-left: 50px">Chromus v3.0.2</h1>\
 \
         <a href="https://github.com/chromus/chromus" style="display:block;margin-top:15px; font-size: 14px;" target="_blank">github</a>\
+\
+        <div>\
+            <label><input type="checkbox" name="debug" {{#if debug}}checked="checked"{{/if}}/> Debug mode</label>            \
+        </div>\
     </div>\
 ');
 
+  About = (function() {
+
+    __extends(About, Backbone.View);
+
+    function About() {
+      About.__super__.constructor.apply(this, arguments);
+    }
+
+    About.prototype.events = {
+      "change input[name='debug']": 'toggleDebug'
+    };
+
+    About.prototype.initialize = function() {
+      chromus.openPanel(this.el);
+      return this.render();
+    };
+
+    About.prototype.render = function() {
+      var view;
+      view = {
+        debug: store.get('debug')
+      };
+      return this.el.innerHTML = template(view);
+    };
+
+    About.prototype.toggleDebug = function(el) {
+      return store.set('debug', el.checked);
+    };
+
+    return About;
+
+  })();
+
   about = $('<li>About</li>').bind('click', function() {
     $('#main_menu').hide();
-    return chromus.openPanel(template());
+    return new About();
   });
 
   chromus.addMenu(about);
