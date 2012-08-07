9e336a42daa22479d30874fe83978399ac243bca
Leonid Bugaev <leonsbox@gmail.com>
2012-08-07 17:37:44 +0600

    Fixer to work with new extension policies

---
+++ javascript/ga.js
@@ -0,0 +1,13 @@
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-16810054-3']);
+      _gaq.push(['_trackPageview']);
+      _gaq.push(['_trackEvent', 'pageLoad', 'background', '2.8.20']);      
+
+      if(BrowserDetect.OS != 'Mac' && BrowserDetect.OS != 'Linux'){      
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = 'https://ssl.google-analytics.com/ga.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+        })();
+      }
